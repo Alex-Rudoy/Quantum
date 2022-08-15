8b380a73cb4063aa076325eea80eb1219fe39ec5
Alex Rudoy <rudoy4ik@gmail.com>
2022-08-15 15:58:03 +0300

    test orientation 2

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -4,16 +4,15 @@ console.log(backgrounds);
 
 document.addEventListener("mousemove", (event) => {
   backgrounds.forEach((bg) => {
-    // move background based on mouse position
-    bg.style.transform = `translate(${event.clientX / 10}px, ${
-      event.clientY / 10
-    }px)`;
+    bg.style.transform = `translate(${
+      event.clientX / 10 - window.innerWidth / 20
+    }px, ${event.clientY / 10 - window.innerHeight / 20}px)`;
   });
 });
 
 // move background based on mobile phone tilt
 window.addEventListener("deviceorientation", (event) => {
   backgrounds.forEach((bg) => {
-    bg.style.transform = `translate(${event.gamma}px, ${event.beta}px)`;
+    bg.style.transform = `translate(${event.gamma}px, ${event.beta - 35}px)`;
   });
 });
