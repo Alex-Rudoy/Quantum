71ed2d71320cbc1249252f7eac2ab8fdf78fbd78
Alex Rudoy <rudoy4ik@gmail.com>
2022-08-15 13:50:15 +0300

    colors, blobserver

--- src/js/intersectionObserver.js
+++ src/js/intersectionObserver.js
@@ -1,3 +1,4 @@
+let goalsShown = 0;
 const observer = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach((entry) => {
@@ -10,39 +11,56 @@ const observer = new IntersectionObserver(
         }
 
         if (entry.target.classList.contains("goals_1")) {
-          document
-            .querySelector(".quarter_1")
-            .classList.add("visible", "active");
+          goalsShown = Math.max(goalsShown, 1);
+          document.querySelector(".quarter_1").classList.add("visible");
           document.querySelector(".dot_1").classList.add("visible");
           document.querySelector(".line").classList.add("line_1");
         }
 
         if (entry.target.classList.contains("goals_2")) {
-          document.querySelector(".quarter_1").classList.remove("active");
-          document
-            .querySelector(".quarter_2")
-            .classList.add("visible", "active");
+          goalsShown = Math.max(goalsShown, 2);
+          document.querySelector(".quarter_2").classList.add("visible");
           document.querySelector(".dot_2").classList.add("visible");
           document.querySelector(".line").classList.add("line_2");
         }
 
         if (entry.target.classList.contains("goals_3")) {
-          document.querySelector(".quarter_1").classList.remove("active");
-          document.querySelector(".quarter_2").classList.remove("active");
-          document
-            .querySelector(".quarter_3")
-            .classList.add("visible", "active");
+          goalsShown = Math.max(goalsShown, 3);
+          document.querySelector(".quarter_3").classList.add("visible");
           document.querySelector(".dot_3").classList.add("visible");
           document.querySelector(".line").classList.add("line_3");
         }
+
+        document.querySelector(".quarter_1").classList.remove("active");
+        document.querySelector(".quarter_2").classList.remove("active");
+        document.querySelector(".quarter_3").classList.remove("active");
+        document
+          .querySelector(`.quarter_${goalsShown}`)
+          .classList.add("active");
       }
     });
   },
   {
     threshold: 0.5,
-    rootMargin: "0px 1000px 0px 1000px",
+  }
+);
+
+const blobserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
   }
 );
 
 const toAnimate = document.querySelectorAll(".toAnimate");
 toAnimate.forEach((item) => observer.observe(item));
+
+const blobs = document.querySelectorAll(".blob");
+blobs.forEach((item) => blobserver.observe(item));
