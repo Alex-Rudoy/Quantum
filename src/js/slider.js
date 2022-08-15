a232207b86b50129decfe28df321bfab75963154
Alex Rudoy <rudoy4ik@gmail.com>
2022-08-15 15:26:43 +0300

    slider

---
+++ src/js/slider.js
@@ -0,0 +1,28 @@
+const slider = document.querySelector(".slider");
+
+const prevButton = document.getElementById("slider_button_left");
+const nextButton = document.getElementById("slider_button_right");
+
+let currentSlide = 1;
+
+const animateSlider = () => {
+  const slideToMove = document.getElementById(`slide${currentSlide}`);
+
+  prevButton.classList.remove("disabled");
+  nextButton.classList.remove("disabled");
+  if (currentSlide === 1) prevButton.classList.add("disabled");
+  if (currentSlide === 3) nextButton.classList.add("disabled");
+  slider.style.transform = `translateX(-${slideToMove.offsetLeft}px)`;
+};
+
+prevButton.addEventListener("click", () => {
+  if (currentSlide === 1) return;
+  currentSlide--;
+  animateSlider();
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentSlide === 3) return;
+  currentSlide++;
+  animateSlider();
+});
