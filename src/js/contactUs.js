e716c9708a7f6f72daa408cbfe04a8dc0c65d4f8
Alex Rudoy <rudoy4ik@gmail.com>
2022-08-24 23:51:39 +0300

    contact us

--- src/js/contactUs.js
+++ src/js/contactUs.js
@@ -2,12 +2,16 @@ const contactUsPopup = document.querySelector(".contact_us_popup");
 const nameField = document.getElementById("name");
 const emailField = document.getElementById("email");
 const messageField = document.getElementById("message");
+const sendButton = document.querySelector(".send_feedback_button");
+const feedbackResult = document.querySelector(".feedback_result");
 
 const openPopup = () =>
   contactUsPopup.classList.add("contact_us_popup_visible");
 
-const closePopup = () =>
+const closePopup = () => {
   contactUsPopup.classList.remove("contact_us_popup_visible");
+  feedbackResult.innerHTML = "";
+};
 
 document.querySelectorAll(".contact_us_button").forEach((button) => {
   button.addEventListener("click", openPopup);
@@ -24,15 +28,38 @@ document
   });
 
 // send data to email
-document
-  .querySelector(".send_feedback_button")
-  .addEventListener("click", async () => {
-    closePopup();
+sendButton.addEventListener("click", async () => {
+  try {
+    sendButton.disabled = true;
+    const res = await fetch(
+      "https://quantum-gravity-back.herokuapp.com/api/qgfeedback",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameField.value,
+          email: emailField.value,
+          message: messageField.value,
+        }),
+      }
+    );
 
-    // todo: change to send email
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    if (res.status !== 200) throw new Error();
+
+    feedbackResult.innerHTML = "Thank you for your feedback!";
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     nameField.value = "";
     emailField.value = "";
     messageField.value = "";
-  });
+    closePopup();
+  } catch (error) {
+    feedbackResult.innerHTML =
+      "Something went wrong, email us at m.elementies@gmail.com";
+  } finally {
+    sendButton.disabled = false;
+  }
+});
